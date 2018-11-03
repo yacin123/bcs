14de7c57253dc0077046d12a8df21f36ee269fa6
yacin123 <44523421+yacin123@users.noreply.github.com>
2018-11-04 00:21:12 +0100

    Update bot (3).js

--- bot (3).js
+++ bot (3).js
@@ -1,31 +1,10 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '*'
-
-client.on('ready', () => { //console.log
-  console.log('--------------------------');
-  console.log(' Bot Is Online')
-  console.log('--------------------------')
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('--------------------------')
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('--------------------------')
-  console.log(`servers! * [ " ${client.guilds.size} " ]`);
-  console.log('--------------------------')
-  console.log(` Users! * [ " ${client.users.size} " ]`);
-  console.log('--------------------------')
-  console.log(`channels! * [ " ${client.channels.size} " ]`);
- 
-  });
-
- 
-
 client.on('message',async message => {
   if(message.author.bot || message.channel.type === 'bc') return;
   let args = message.content.split(' ');
   if(args[0] === `${prefix}bc`) {
     if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
     if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
+
   
     let msgCount = 0;
     let errorCount = 0;
@@ -46,61 +25,23 @@ client.on('message',async message => {
   }
 });
 
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
- 
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 100,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
----------------------
--[${message.guild.name}]  هذا هو رابط سيرفر
----------------------
--هذا الرابط صالح ل 100 مستخدم فقط
----------------------
--هذا الرابط صالح لمده 24 ساعه فقط
----------------------
-**`)
-      message.author.sendEmbed(Embed11)
-    }
-});
-
-
-client.on('message', message => {
-    if(message.content.includes('discord.gg')){
-                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? 🤔   **');
-        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-        message.delete()
-    return message.reply(`** No Invite Links :angry: !**`)
-    }
+    
+Attachment file type: unknown
+Procfile
+196 bytes
+Attachment file type: code
+bot.js
+4.10 KB
+{
+  "name": "Test",
+  "description": "Test",
+  "version": "0.0.0",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "dependencies": {
+    "discord.js": "^11.3.2",
+    "request": "2.81.0"
+  }
 }
-});
-
-client.on('ready', () => { //playing
-    client.user.setGame(`**#North Network 1k**`,'https://www.twitch.tv/clo_wiin');
-    client.user.setStatus('Online')
-});
-
-client.on('guildMemberAdd',member=>{
-  if(member.guild.id !== "505076617945808897") return;
-setTimeout(() => {
-
-  client.channels.get("505076713441460234").sendMessage("**#Welcome to __North Network__ **");
-},1500);
-
-});
-
-
-client.login(process.env.BOT_TOKEN2);
